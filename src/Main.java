99ca0d905627835cd147529e120a5dacac477a9b
Tatyana58 <tatbir@rambler.ru>
2022-11-21 20:12:15 +0300

    Beginning Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашняя работ №4 Циклы");
-
-
-
-
+        //Задание 1
+        //С помощью цикла for выведите в консоль все числа от 1 до 10.
+        for (int i = 1; i < 10; i++) ;
+        {
+            System.out.print(i + "Число = ");
+        }
     }
-}
+    }
