5ea43abb26e041fdae4642575355184a5b4aeb71
Tatyana58 <tatbir@rambler.ru>
2022-11-21 20:23:32 +0300

    Beginning Task 1 Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Домашняя работ №4 Циклы");
+    public static void main(String[] args) {System.out.println("Домашняя работ №4 Циклы");
         //Задание 1
         //С помощью цикла for выведите в консоль все числа от 1 до 10.
-        for (int i = 1; i < 10; i++) ;
+        for (int i=1; i<11; i++)
         {
-            System.out.print(i + "Число = ");
+           System.out.println("Число = "+i);
+        }
+        System.out.println("");
+        //Задание 2
+        //С помощью цикла for выведите в консоль все числа от 10 до 1.
+        for (int i=10; i>0; i--)
+        {
+            System.out.println("Число = "+i);
         }
     }
-    }
+}
